20e5a8e462645a7b7381c651bf8508e87b96c309
omartrese <omarlayachi13@gmail.com>
2025-06-12 19:28:24 +0200

    Refactor layout and styles for game and character pages; improve responsiveness and structure

--- src/routes/info/+layout.svelte
+++ src/routes/info/+layout.svelte
@@ -5,12 +5,12 @@
 </script>
 
 <main>
-  <header class="flex flex-row justify-between">
+  <header class="flex flex-row justify-around gap-5 backdrop-blur-sm py-5 text-xl ">
     <div>
-      <h1>Welcome to the Zelda Info Page</h1>
+      <a href="/"><h1 class="font-bold">TLOZ App</h1></a>
     </div>
-    <nav>
-      <ul>
+    <nav class="max-sm:hidden">
+      <ul class="flex flex-row gap-5 *:font-semibold">
         <li><a href="/info/games">Games</a></li>
         <li><a href="/info/characters">Characters</a></li>
         <li><a href="/info/items">Items</a></li>
--- src/routes/info/characters/+page.svelte
+++ src/routes/info/characters/+page.svelte
@@ -21,8 +21,7 @@
 <article>
   <section>
     {#each data as game}
-      <div class="game-card">
-        <h1>culo</h1>
+      <div class="item-card">
         <h2>{game.name}</h2>
         <p>Release Date: {game.released_date}</p>
         <p>{game.description}</p>
@@ -38,9 +37,29 @@
 
   article section {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-template-columns: repeat(3,1fr);
     gap: 20px;
     padding: 20px;
     overflow-y: scroll;
+  }
+
+  .item-card {
+    background-color: #c3d5c8;
+    backdrop-filter: blur(2px);
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease;
+  }
+
+  @media (max-width: 900px) {
+    article section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  @media (max-width: 600px) {
+    article section {
+      grid-template-columns: 1fr;
+    }
   }
 </style>
